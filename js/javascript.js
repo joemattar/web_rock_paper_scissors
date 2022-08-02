c2368554139d25646392a0d35c1202685adb62b5
Joe M <joemattar@gmail.com>
2022-08-02 14:44:20 -0400

    define getComputerChoice function

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,17 @@
-console.log("Hello World!")
+function getComputerChoice () {
+    let randomNumber = Math.floor(Math.random()*3) + 1;
+    let randomChoice = "";
+    if (randomNumber === 1) {
+        randomChoice = "Rock";
+    } else if (randomNumber === 2) {
+        randomChoice = "Paper";
+    } else {
+        randomChoice = "Scissors"
+    }
+    return randomChoice
+}
+
+
+for (let i = 1; i < 50; i++) {
+    console.log(getComputerChoice())
+}
