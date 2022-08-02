c2847a8b171826bdfb8171c7f937ab1ebda3db57
Joe M <joemattar@gmail.com>
2022-08-02 15:28:02 -0400

    define game function

--- js/javascript.js
+++ js/javascript.js
@@ -6,39 +6,64 @@ function getComputerChoice () {
     } else if (randomNumber === 2) {
         randomChoice = "Paper";
     } else {
-        randomChoice = "Scissors"
+        randomChoice = "Scissors";
     }
     return randomChoice
 }
 
 function playRound (playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase()
-    computerSelection = computerSelection.toLowerCase()
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
     if (playerSelection === computerSelection) {
-        return ("It's A Draw!")
+        console.log("It's A Draw!");
+        return "draw"
     } else {
         if (playerSelection === "rock") {
             if (computerSelection === "paper") {
-                return ("You Lose! Paper beats Rock")
+                console.log("You Lose! Paper beats Rock");
+                return "lose"
             } else {
-                return ("You Win! Rock beats Scissors")
+                console.log("You Win! Rock beats Scissors");
+                return "win"
             }
         } else if (playerSelection === "paper") {
             if (computerSelection === "rock") {
-                return ("You Win! Paper beats Rock!")
+                console.log("You Win! Paper beats Rock!");
+                return "win"
             } else {
-                return ("You Lose! Scissors beat Paper")
+                console.log("You Lose! Scissors beat Paper");
+                return "lose"
             }
         } else {
             if (computerSelection === "rock") {
-                return ("You Lose! Rock beats Scissors")
+                console.log("You Lose! Rock beats Scissors");
+                return "lose"
             } else {
-                return ("You Win! Scissors beat Paper")
+                console.log("You Win! Scissors beat Paper");
+                return "win"
             }
         }
     }
 }
 
-for (let i = 1; i < 50; i++) {
-    console.log(playRound(getComputerChoice(), getComputerChoice()))
+function game () {
+    let player_score = 0;
+    let computer_score = 0;
+    while (player_score < 5 && computer_score < 5) {
+        playerChoice = prompt("Please enter your choice ('Rock' / 'Paper' / Scissors'): ").toLowerCase();
+        let result = playRound(playerChoice, getComputerChoice());
+        if (result === "win") {
+            player_score += 1;
+        } else if (result === "lose") {
+            computer_score += 1;
+        }
+        console.log(`The current score is: PLAYER ${player_score} - ${computer_score} COMPUTER`);
+    }
+    if (player_score === 5) {
+        console.log("GAME OVER! YOU WIN!");
+    } else {
+        console.log("GAME OVER! YOU LOSE!");
+    }
 }
+
+game();
