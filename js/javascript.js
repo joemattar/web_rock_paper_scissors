991a8c5c35cf7b88dccd6546858c347f6216191a
Joe M <joemattar@gmail.com>
2022-08-09 16:13:09 -0400

    JS Create displayComputerSelection function exit

--- js/javascript.js
+++ js/javascript.js
@@ -9,44 +9,36 @@ const playerDiv = document.querySelector("div.player");
 const computerDiv = document.querySelector("div.computer");
 const playerImg = document.querySelector("img.player");
 const computerImg = document.querySelector("img.computer");
-
-// User is either Player or Computer
-// Returns a random computer choice. Independent of GUI
-function getComputerChoice () {
-    let randomNumber = Math.floor(Math.random()*3) + 1;
-    let randomChoice = "";
-    if (randomNumber === 1) {
-        randomChoice = "rock";
-    } else if (randomNumber === 2) {
-        randomChoice = "paper";
-    } else {
-        randomChoice = "scissors";
-    }
-    return randomChoice
-}
+const spanRound = document.querySelector("span.round");
+const spanPlayerScore = document.querySelector("span.player");
+const spanComputerScore = document.querySelector("span.computer");
+const spanMake_choice = document.querySelector("span.make_choice");
 
 // Displays user selection in the respective img tag of div .resolution
 function displayPlayerSelection (playerSelection) {
     if (playerSelection === "rock") {
-        userImg.src = "./images/hand_rock.png";
+        playerImg.src = "./images/hand_rock.png";
         displaySelectionEffect(playerDiv);
         playerChoice = "rock";
-        } else if (playerSelection === "paper") {
-        userImg.src = "./images/hand_paper.png";
+    } else if (playerSelection === "paper") {
+        playerImg.src = "./images/hand_paper.png";
         displaySelectionEffect(playerDiv);
         playerChoice = "paper";
     } else if (playerSelection === "scissors") {
-        userImg.src = "./images/hand_scissors.png";
+        playerImg.src = "./images/hand_scissors.png";
         displaySelectionEffect(playerDiv);
         playerChoice = "scissors";
     }
-    disableButtons();    
+    disableButtons();
+    // The timout below is related to the transition time in .resolution div
+    setTimeout(displayComputerSelection, 1000);
+
 }
 
 //.resolution div selection effect
-function displaySelectionEffect (div) {
-    div.classList.add("selecting")
-    div.addEventListener("transitionend", () => div.classList.remove("selecting"))
+function displaySelectionEffect (tag) {
+    tag.classList.add("selecting")
+    tag.addEventListener("transitionend", () => tag.classList.remove("selecting"))
 }
 
 // Functions to disable buttons
@@ -63,17 +55,41 @@ function enableButtons() {
     scissorsButton.disabled = false;
 }
 
+// Returns a random computer choice. Independent of GUI
+function getComputerChoice () {
+    let randomNumber = Math.floor(Math.random()*3) + 1;
+    let randomChoice = "";
+    if (randomNumber === 1) {
+        randomChoice = "rock";
+    } else if (randomNumber === 2) {
+        randomChoice = "paper";
+    } else {
+        randomChoice = "scissors";
+    }
+    return randomChoice
+}
+
+function displayComputerSelection() {
+    computerChoice = getComputerChoice();
+    computerImg.src = `./images/hand_${computerChoice}.png`;
+    displaySelectionEffect(computerDiv);
+
+}
 
 
-rockButton.addEventListener("click", displayPlayerSelection.bind(null, user="player", playerSelection="rock"));        
-paperButton.addEventListener("click", displayPlayerSelection.bind(null, user="player", playerSelection="paper"));
-scissorsButton.addEventListener("click", displayPlayerSelection.bind(null, user="player", playerSelection="scissors"));
+
+
+
+
+
+rockButton.addEventListener("click", displayPlayerSelection.bind(null, playerSelection="rock"));        
+paperButton.addEventListener("click", displayPlayerSelection.bind(null, playerSelection="paper"));
+scissorsButton.addEventListener("click", displayPlayerSelection.bind(null, playerSelection="scissors"));
 
 
 
 
 function playRound (playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
     if (playerSelection === computerSelection) {
         displaySelections(playerSelection, computerSelection)
         console.log("It's A Draw!");
@@ -81,40 +97,30 @@ function playRound (playerSelection, computerSelection) {
     } else {
         if (playerSelection === "rock") {
             if (computerSelection === "paper") {
-                displaySelections(playerSelection, computerSelection)
                 console.log("You Lose! Paper beats Rock");
                 return "lose"
             } else {
-                displaySelections(playerSelection, computerSelection)
                 console.log("You Win! Rock beats Scissors");
                 return "win"
             } 
         } else if (playerSelection === "paper") {
             if (computerSelection === "rock") {
-                displaySelections(playerSelection, computerSelection)
                 console.log("You Win! Paper beats Rock!");
                 return "win"
             } else {
-                displaySelections(playerSelection, computerSelection)
                 console.log("You Lose! Scissors beat Paper");
                 return "lose"
             }
         } else {
             if (computerSelection === "rock") {
-                displaySelections(playerSelection, computerSelection)
                 console.log("You Lose! Rock beats Scissors");
                 return "lose"
             } else {
-                displaySelections(playerSelection, computerSelection)
                 console.log("You Win! Scissors beat Paper");
                 return "win"
             }
         }
     }
-}
-
-function displaySelections (playerSelection, computerSelection) {
-    console.log(`PLAYER chose: ${playerSelection.toUpperCase()} - COMPUTER chose: ${computerSelection.toUpperCase()}`)
 }
 
 function game () {
